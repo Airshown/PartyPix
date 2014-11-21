b46c94ee661a0867f3a2b28be48484b5af6513bf
Airshown <mr.erdah@gmail.com>
2014-11-21 11:33:39 +0100

    Test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 
 <html lang="fr-FR" class="no-js">
+
    <head>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -130,4 +131,5 @@
          })();
       </script>
    </body>
+   
 </html>
